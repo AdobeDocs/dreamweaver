38e1d4e09c807a26c923a5a1b5a93615d840f776
Finnegan <finnegan@adobe.com>
2020-12-03 15:32:46 -0500

    ignoring DS_Store

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,5 +28,6 @@ module.exports = {
     ]
   },
   plugins: [`@adobe/gatsby-theme-aio`],
-  pathPrefix: process.env.PATH_PREFIX || '/dreamweaver'
+  pathPrefix: process.env
+**/.DS_Store.PATH_PREFIX || '/dreamweaver'
 };
